98cd9b10a03b204f8bf1249d8910577dd4ac5f94
Elias Alejandro Diaz Cahuas <N00369765@upn.pe>
2023-10-07 01:25:32 -0500

    Agregar archivos de proyecto.

---
+++ Parcial (sem 8) - Problema 3/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("\tAlgoritmo para mostrar asteríscos escalonados");
+Console.WriteLine("\t*********************************************\n");
+Console.Write(" Ingrese un número: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int TerminoAnterior = 0;
+
+for (int i = 0; i < num; i++)
+{
+    int suma = TerminoAnterior + i;
+    TerminoAnterior = i;
+    if (suma > 0)
+    {
+        for (int j = 1; j <= suma; j++)
+        {
+            Console.Write("*");            
+        }
+    }
+    Console.WriteLine("*");
+}
